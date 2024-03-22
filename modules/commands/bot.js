cd399c85ee754309aaab8d517e8390dad6e8280a
ziddiqueen2345 <164534606+ziddiqueen2345@users.noreply.github.com>
2024-03-22 22:47:18 +0530

    Update bot.js

--- modules/commands/bot.js
+++ modules/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🚧${name}🚧,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🚧${name}🚧,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𒁍≛⃝𝗭𝗶𝗱𝗱𝗶 𝗤𝘂𝗲𝗲𝗻🥀💔🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
