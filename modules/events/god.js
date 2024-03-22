7951c9045855048d7aa88df4fafe4b6402b0fd4e
ziddiqueen2345 <164534606+ziddiqueen2345@users.noreply.github.com>
2024-03-22 22:43:33 +0530

    Update god.js

--- modules/events/god.js
+++ modules/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100040426712109";
+  var god = "100084944927647";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
